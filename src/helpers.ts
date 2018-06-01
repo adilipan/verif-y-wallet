a508933ed750a3ea26da8db75d3497b9cf528ec0
Arun <arundilipan@gmail.com>
2018-06-01 18:03:05 -0400

    refactor(helpers.ts): Added dictionaries for Fiat and Cryptos
    Added dictionaries to help conversion between Fiat currencies and cryptocurrencies

---
+++ src/helpers.ts
@@ -0,0 +1,50 @@
+export namespace Currencies {
+  const Cryptos = {
+    BTC: { prefix: '\u0E3F', name: 'Bitcoin', bannerName: 'BTC', index: 0 },
+    ETH: { prefix: '\u039E', name: 'Ethereum', bannerName: 'ETH', index: 1 },
+    DAO: { prefix: '\u0110', name: 'The DAO', bannerName: 'DAO', index: 2 },
+    DASH: { prefix: '\u2145', name: 'Dash', bannerName: 'DSH', index: 3 },
+    ETC: { prefix: '\u039E', name: 'Ethereum Classic', bannerName: 'ETC', index: 4 },
+    REP: { prefix: '\u024C', name: 'Augur', bannerName: 'REP', index: 5 },
+    LTC: { prefix: '\u0141', name: 'Litecoin', bannerName: 'LTC', index: 6 },
+    LSK: { prefix: '\u2C60', name: 'Lisk', bannerName: 'LSK', index: 7 },
+    ZEC: { prefix: '\u24E9', name: 'ZCash', bannerName: 'ZEC', index: 8 },
+    TESTNET_RSK: { prefix: '\uc98c', name: 'Rootstock Testnet', bannerName: 'RSK', index: 9 }
+  }
+
+  const Fiat = {
+    AUD: { prefix: 'AU$', name: 'Australian Dollar' },
+    BRL: { prefix: 'R$', name: 'Brazilian Real' },
+    CAD: { prefix: 'CA$', name: 'Canadian Dollar' },
+    CHF: { prefix: '\u0043', name: 'Swiss Franc' },
+    CLP: { prefix: 'CL$', name: 'Chilean Peso' },
+    CNY: { prefix: '\u00A5', name: 'Chinese Yuan' },
+    CZK: { prefix: '\u004b', name: 'Czech Republic Koruna' },
+    DKK: { prefix: 'kr', name: 'Danish Krona' },
+    EUR: { prefix: '\u20AC', name: 'Euro' },
+    GBP: { prefix: '\u00A3', name: 'British Pound' },
+    HKD: { prefix: 'HK$', name: 'Hong Kong Dollar' },
+    HUF: { prefix: '\u0046', name: 'Hungarian Forint' },
+    IDR: { prefix: '\u0052', name: 'Indonesian Rupiah' },
+    ILS: { prefix: '\u20AA', name: 'Israeli New Shekel' },
+    INR: { prefix: '\u20B9', name: 'Indian Rupee' },
+    ISK: { prefix: 'kr', name: 'Icelandik Kroner' },
+    JPY: { prefix: '\u00A5', name: 'Japanese Yen' },
+    KRW: { prefix: '\u20A9', name: 'South Korean Won' },
+    MXN: { prefix: 'MX$', name: 'Mexican Peso' },
+    MYR: { prefix: 'RM', name: 'Malaysian Myr' },
+    NOK: { prefix: 'kr', name: 'Norwegian Kroner' },
+    NZD: { prefix: 'NZ$', name: 'New Zealand Dollar' },
+    PHP: { prefix: '\u20B1', name: 'Phillipine Peso' },
+    PKR: { prefix: '\u20A8', name: 'Pakistani Rupee' },
+    PLN: { prefix: 'z\u0142', name: 'Polish Zlotty' },
+    RUB: { prefix: '\u20BD', name: 'Russian Ruble' },
+    SEK: { prefix: 'kr', name: 'Swedish Krona' },
+    SGD: { prefix: 'SG$', name: 'Singapore Dollar' },
+    THB: { prefix: '\u0e3f', name: 'Thailand Baht' },
+    TRY: { prefix: 't', name: 'Turkey Lira' },
+    TWD: { prefix: 'NT$', name: 'New Taiwan Dollar' },
+    USD: { prefix: 'US$', name: 'United States Dollar' },
+    ZAR: { prefix: '\u0052', name: 'South African Rand' }
+  }
+}
--- src/pouch.ts
+++ src/pouch.ts
@@ -1,37 +1,22 @@
 import { IFold } from './fold'
 
 export interface IPouch {
-  name: string
-  symbol: string
-  conversionRate: number
-  conversionFee: number
-  totalAmount: number
-  totalAmountInFiat: number
+  fullName: string
+  fullDisplayName: string
+  abbreviation: string
+  hdType: number
+  isSubtype: boolean
+  isTestnet: boolean
+  mnemonic: string
   folds: IFold[]
-
-  convertFromFiat(amount: number): number
 }
 
-export class Pouch implements IPouch {
-  name: string
-  symbol: string
-  conversionRate: number
-  conversionFee: number
-  totalAmount: number
-  totalAmountInFiat: number
-  folds: IFold[]
-
-  constructor() {
-    this.name = 'empty'
-    this.symbol = 'empty'
-    this.conversionFee = 0
-    this.conversionRate = 1
-    this.totalAmount = 0
-    this.totalAmountInFiat = 0
-    this.folds = []
-  }
-
-  convertFromFiat(amount: number): number {
-    return (amount - this.conversionFee) / this.conversionRate
+export class Pouch {
+  derive(node: Node, index: Index, hardened: boolean): IPouch {
+    if (hardened) {
+      return node.deriveHardened(index)
+    } else {
+      return node.derive(index)
+    }
   }
 }
